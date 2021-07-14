3d4871e5d15d327e40d978912b2039d7300e3130
yi1.li <yi1.li@intel.com>
2021-07-14 12:49:04 +0800

    Add dump tools use-guid and fix hard code

--- Tools/ManifestTools/LinuxSpdmTools/fsp-attestation.sh
+++ Tools/ManifestTools/LinuxSpdmTools/fsp-attestation.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
 # fsp attestation
+#
+
 FSPtools_path="../"
 Image_bin="$1"
 Eventlog_bin="$2"
 Out_xml="spdm_swid_temp.xml"
+#echo color:
+GREEN='\E[1;32m'
+YELOW='\E[1;33m'
+BLUE='\E[1;34m'
+PINK='\E[1;35m'
+RES='\E[0m' 
 
-echo  -e "\033[34m -------------------------------------- \033[0m"
-echo  -e "\033[34m |FSP TOOLS start ...                 | \033[0m"
-echo  -e "\033[34m |genswid start ...                   | \033[0m"
-echo  -e "\033[34m -------------------------------------- \033[0m"
+echo  -e "${BLUE} -------------------------------------- ${RES}"
+echo  -e "${BLUE} |FSP TOOLS start ...                 | ${RES}"
+echo  -e "${BLUE} |genswid start ...                   | ${RES}"
+echo  -e "${BLUE} -------------------------------------- ${RES}"
 python $FSPtools_path/FspGenSwid.py genswid -i $FSPtools_path/SampleConfig/FspRimTemplate.ini -p $Image_bin -t SHA_256 -o $FSPtools_path/$Out_xml
 sleep 1
 echo  "$FSPtools_path/$Out_xml"
-echo  -e "\033[34m -------------------------------------- \033[0m"
-echo  -e "\033[34m |genswid end   ...                   | \033[0m"
-echo  -e "\033[34m -------------------------------------- \033[0m"
+echo  -e "${BLUE} -------------------------------------- ${RES}"
+echo  -e "${BLUE} |genswid end   ...                   | ${RES}"
+echo  -e "${BLUE} -------------------------------------- ${RES}"
 
-echo  -e "\033[34m -------------------------------------- \033[0m"
-echo  -e "\033[34m |verify-hash start  ...              | \033[0m"
-echo  -e "\033[34m -------------------------------------- \033[0m"
+echo  -e "${BLUE} -------------------------------------- ${RES}"
+echo  -e "${BLUE} |verify-hash start  ...              | ${RES}"
+echo  -e "${BLUE} -------------------------------------- ${RES}"
 python  $FSPtools_path/FspGenSwid.py verify-hash -f  $FSPtools_path/$Out_xml -t SHA_256  --evt $Eventlog_bin
 sleep 1
-echo  -e "\033[34m -------------------------------------- \033[0m"
-echo  -e "\033[34m |verify-hash end    ...              | \033[0m"
-echo  -e "\033[34m -------------------------------------- \033[0m"
-echo  -e "\033[34m -------------------------------------- \033[0m"
-echo  -e "\033[34m |compare start ...                   | \033[0m"
-echo  -e "\033[34m -------------------------------------- \033[0m"
+echo  -e "${BLUE} -------------------------------------- ${RES}"
+echo  -e "${BLUE} |verify-hash end    ...              | ${RES}"
+echo  -e "${BLUE} -------------------------------------- ${RES}"
+echo  -e "${BLUE} -------------------------------------- ${RES}"
+echo  -e "${BLUE} |compare start ...                   | ${RES}"
+echo  -e "${BLUE} -------------------------------------- ${RES}"
 
 python  $FSPtools_path/FspTools.py compare --evt $Eventlog_bin --fd $Image_bin
 
 
-echo  -e "\033[34m -------------------------------------- \033[0m"
-echo  -e "\033[34m |compare end   ...                   | \033[0m"
-echo  -e "\033[34m -------------------------------------- \033[0m"
+echo  -e "${BLUE} -------------------------------------- ${RES}"
+echo  -e "${BLUE} |compare end   ...                   | ${RES}"
+echo  -e "${BLUE} -------------------------------------- ${RES}"
